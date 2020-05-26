4904fddad86bc57faaeae37ac75fb6240f6868e0
Chrisctr <credfern96@gmail.com>
2020-05-26 17:01:32 +0100

    Adding to CustomersServices

--- src/main/java/com/qa/cineverse/service/CustomersService.java
+++ src/main/java/com/qa/cineverse/service/CustomersService.java
@@ -2,11 +2,14 @@ package com.qa.cineverse.service;
 
 import com.qa.cineverse.domain.Customers;
 import com.qa.cineverse.dto.CustomersDTO;
+import com.qa.cineverse.exception.CustomersNotFoundException;
 import com.qa.cineverse.repo.CustomersRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -26,7 +29,23 @@ public class CustomersService {
         return this.mapper.map(customers, CustomersDTO.class);
     }
 
+    public List<CustomersDTO> readCustomers(){
+        return this.customersRepo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
 
+    public CustomersDTO createCustomers(Customers customers){
+        return this.mapToDTO(this.customersRepo.save(customers));
+    }
 
+    public CustomersDTO findCustomersById(Long id){
+        return this.mapToDTO(this.customersRepo.findById(id).orElseThrow(CustomersNotFoundException::new));
+    }
+
+    public CustomersDTO updateCustomers(Long id, Customers customers){
+        Customers update = this.customersRepo.findById(id).orElseThrow(CustomersNotFoundException::new);
+        update.setName(customers.getName());
+        Customers tempCustomers = this.customersRepo.save(update);
+        return this.mapToDTO(tempCustomers);
+    }
 
 }
