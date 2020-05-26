fd9ae67be3db3f6ce6dc5ccac819a85ec36fc065
Chrisctr <credfern96@gmail.com>
2020-05-26 16:46:04 +0100

    Created CustomersController

--- src/main/java/com/qa/cineverse/controller/CustomersController.java
+++ src/main/java/com/qa/cineverse/controller/CustomersController.java
@@ -1,7 +1,18 @@
 package com.qa.cineverse.controller;
 
+import com.qa.cineverse.service.CustomersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomersController {
+
+    private final CustomersService service;
+
+    @Autowired
+    public CustomersController(CustomersService service) {
+        this.service = service;
+    }
+
+
 }
