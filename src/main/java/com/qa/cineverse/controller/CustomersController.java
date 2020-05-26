7b907a905c48df1fa41ab28a0207b109899755cc
Chrisctr <credfern96@gmail.com>
2020-05-26 17:04:04 +0100

    Adding to CustomersController

--- src/main/java/com/qa/cineverse/controller/CustomersController.java
+++ src/main/java/com/qa/cineverse/controller/CustomersController.java
@@ -1,8 +1,14 @@
 package com.qa.cineverse.controller;
 
+import com.qa.cineverse.domain.Customers;
+import com.qa.cineverse.dto.CustomersDTO;
 import com.qa.cineverse.service.CustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CustomersController {
@@ -14,5 +20,25 @@ public class CustomersController {
         this.service = service;
     }
 
+    @GetMapping("/getAllCustomers")
+    public ResponseEntity<List<CustomersDTO>> getAllCustomers(){
+        return ResponseEntity.ok(this.service.readCustomers ());
+    }
 
+    @PostMapping("/createCustomers")
+    public ResponseEntity<CustomersDTO> createCharacter(@RequestBody Customers customers){
+        return new ResponseEntity<>(this.service.createCustomers (customers), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/deleteCustomers/{id}")
+    public ResponseEntity<?> deleteCustomers(@PathVariable Long id){
+        return this.service.deleteCustomers (id)
+                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+                : ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/getCharacterById/{id}")
+    public ResponseEntity<CharacterDTO> getCharacterById(@PathVariable Long id){
+        return ResponseEntity.ok(this.service.findCharacterById (id));
+    }
 }
