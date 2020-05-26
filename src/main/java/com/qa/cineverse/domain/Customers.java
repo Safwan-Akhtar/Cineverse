45062f333e51863c78f5298d7ef41e4be14bda5f
Chrisctr <credfern96@gmail.com>
2020-05-26 16:29:42 +0100

    Added hibernate to pom.xml

---
+++ src/main/java/com/qa/cineverse/domain/Customers.java
@@ -0,0 +1,11 @@
+package com.qa.cineverse.domain;
+
+import org.hibernate.annotations.Proxy;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "character_sheet")
+public class Customers {
+}
