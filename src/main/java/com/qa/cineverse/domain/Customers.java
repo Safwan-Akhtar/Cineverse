7e666e37726db16e0790ee91390332dfa3f27198
Chrisctr <credfern96@gmail.com>
2020-05-26 16:32:02 +0100

    Created customers domain

--- src/main/java/com/qa/cineverse/domain/Customers.java
+++ src/main/java/com/qa/cineverse/domain/Customers.java
@@ -6,6 +6,65 @@ import javax.persistence.*;
 import java.util.*;
 
 @Entity
-@Table(name = "character_sheet")
+@Table(name = "customers")
 public class Customers {
+
+    @Id
+    @GeneratedValue
+    private Long customerId;
+
+    @Column(name = "name")
+    private String name;
+
+    public Customers() {
+    }
+
+    public Customers(String name) {
+        this.name = name;
+    }
+
+    public Customers(Long customerId, String name) {
+        this.customerId = customerId;
+        this.name = name;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Customers))
+            return false;
+        Customers customers = (Customers) o;
+        return getCustomerId ().equals (customers.getCustomerId ()) &&
+                getName ().equals (customers.getName ());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash (getCustomerId (), getName ());
+    }
+
+    @Override
+    public String toString() {
+        return "Customers{" +
+                "customerId=" + customerId +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
