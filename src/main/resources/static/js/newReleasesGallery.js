563cb7a70e605982651f853b8327444331aee4fe
luke <lukesosbourne@gmail.com>
2020-05-27 17:05:15 +0100

    got axios call to work

--- src/main/resources/static/js/newReleasesGallery.js
+++ src/main/resources/static/js/newReleasesGallery.js
@@ -1,36 +1,14 @@
-// const REQ = new XMLHttpRequest();
-// function getMovieById() {
-//
-//     // The King's Man
-//
-//     // REQ.open("GET", `http://www.omdbapi.com/?apikey=c819fc9&i=tt6856242`);
-//     // REQ.setRequestHeader("Content-Type", "Application/json");
-//     // REQ.responseType = "json";
-//     // REQ.send();
-//
-//
-//     REQ.open("GET", "https://imdb8.p.rapidapi.com/title/get-releases?tconst=tt0944947");
-//     REQ.setRequestHeader("x-rapidapi-host", "imdb8.p.rapidapi.com");
-//     REQ.setRequestHeader("x-rapidapi-key", "5177a72475msh5e187a1f0dc4021p1abbffjsn4e135e71f191");
-//     REQ.send(data);
-//
-//     REQ.onload = () => {
-//         if(REQ.status == 200) {
-//             console.dir(REQ);
-//             console.log(REQ.response);
-//             console.log("here is the movie");
-//         } else {
-//             console.log("there was an error in retrieving the data");
-//         }
-//     }
-// }
-// let buttGetMovieById = document.querySelector("#getMovie");
-// buttGetMovieById.addEventListener("click", getMovieById);
-
-function getMovie() {
-
-axios.get(`http://www.omdbapi.com/?apikey=335035be&i=46`)
-    .then(response => response.data ))
-    .catch(err => console.error(err));
-
+function yo () {
+    axios({
+        method: 'get',
+        url: `http://www.omdbapi.com/?apikey=335035be&i=tt3089630`,
+    })
+        .then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error);
+        });
 }
+
+let buttGetMovieById = document.querySelector("#getMovie");
+buttGetMovieById.addEventListener("click", yo);
