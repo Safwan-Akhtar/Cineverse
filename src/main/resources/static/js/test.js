637188bb15295c725b53161b02a1da9234aca677
femarleycode <femarleycode@gmail.com>
2020-05-27 18:27:12 +0100

    fixed 12a and 15, added and finished 18 and r18, all categories are finished

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,7 +1,5 @@
 
-
 document.getElementById('uClick').addEventListener('click', () => {
-    // alert('Button was pressed!');
     var x = document.getElementById("udesc");
     if (x.style.display === "none") {
         x.style.display = "block";
@@ -10,8 +8,9 @@ document.getElementById('uClick').addEventListener('click', () => {
     }
 })
 
+//below can probably be looped
+
 document.getElementById('pgClick').addEventListener('click', () => {
-    // alert('Button was pressed!');
     var x = document.getElementById("pgdesc");
     if (x.style.display === "none") {
         x.style.display = "block";
@@ -21,7 +20,6 @@ document.getElementById('pgClick').addEventListener('click', () => {
 })
 
 document.getElementById('12aClick').addEventListener('click', () => {
-    // alert('Button was pressed!');
     var x = document.getElementById("12adesc");
     if (x.style.display === "none") {
         x.style.display = "block";
@@ -31,8 +29,25 @@ document.getElementById('12aClick').addEventListener('click', () => {
 })
 
 document.getElementById('15Click').addEventListener('click', () => {
-    // alert('Button was pressed!');
     var x = document.getElementById("15desc");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
+
+document.getElementById('18Click').addEventListener('click', () => {
+    var x = document.getElementById("18desc");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
+
+document.getElementById('r18Click').addEventListener('click', () => {
+    var x = document.getElementById("r18desc");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
