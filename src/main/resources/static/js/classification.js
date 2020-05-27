d94f40c41903dffdbe026f521d84d307baaf8d42
femarleycode <femarleycode@gmail.com>
2020-05-27 16:48:34 +0100

    classification changes, images and some failed index formatting

---
+++ src/main/resources/static/js/classification.js
@@ -0,0 +1,18 @@
+function ucatFunc() {
+    var x = document.getElementById("classu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
+
+
+function pgcatFunc() {
+    var x = document.getElementById("pgcat");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
