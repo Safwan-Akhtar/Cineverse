e02c4382a806996cc8a734fbd2af0a7296e2f147
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-28 09:29:07 +0100

    added console log get req

--- src/main/resources/static/js/listingsGallery.js
+++ src/main/resources/static/js/listingsGallery.js
@@ -1,3 +1,8 @@
 const axios = require('axios');
 
 const display = document.querySelector("#currentListingsDisplay");
+
+axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=6b2221c9').then(resp => {
+
+    console.log(resp.data);
+});
