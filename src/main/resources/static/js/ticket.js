61554c7265ce1a065587491ae3eeb00ce9677a94
Chrisctr <credfern96@gmail.com>
2020-05-28 23:58:20 +0100

    Removed unnecessary fields from html for tickets

--- src/main/resources/static/js/ticket.js
+++ src/main/resources/static/js/ticket.js
@@ -1,16 +1,21 @@
 const postBooking = () => {
     let customername = document.getElementById("customername").value;
     let movieTitle = document.getElementById("movieTitle").value;
+    let screeningDate = document.getElementById("screeningDate").value;
+    let screeningTime = document.getElementById("screeningTime").value;
+    let adult = document.getElementById("adult").value;
+    let child = document.getElementById("child").value;
+    let student = document.getElementById("student").value;
+    let seatClass = document.getElementById("seatClass").value;
 
     console.log(customername);
     console.log(movieTitle);
-
-
-
-
-
-
-
+    console.log(screeningDate);
+    console.log(screeningTime);
+    console.log(adult);
+    console.log(child);
+    console.log(student);
+    console.log(seatClass);
 
 }
 
