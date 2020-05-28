fa7a64cc854034bc6c2e733f897294210883c632
femarleycode <femarleycode@gmail.com>
2020-05-28 17:50:17 +0100

    apis working

--- src/main/resources/static/js/testGallary.js
+++ src/main/resources/static/js/testGallary.js
@@ -1,6 +1,4 @@
 
-
-
 /// original test alert function (required scripts at top of the html)
 
 // $(document).ready(() => {
@@ -24,18 +22,34 @@
 
 $(document).ready(() => {
     $("#searchForm").on("submit", (e) => {
-        let searchText = ($("#searchText").val());
+        let searchText = $("#searchText").val();
         getMovies(searchText);
         e.preventDefault();
     });
 });
 
 function getMovies(searchText) {
-    axios.get("http://www.omdbapi.com?s="movieId + "&apikey=thewdb")
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((err) => {
-        console.log(err);
+    axios.get(`http://www.omdbapi.com/?apikey=c737e3a5&t=`+searchText)
+        .then((response) => {
+            console.log(response);
+            let movies = response.data.Search;
+            let output = '';
+            //underscore removes an error message
+            $.each(movies = (_testGallary, movie) => {
+                output += `
+                <div classes="col-md-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5>${movie.Title}</h5>
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>
+                </div>
+                `;
+            });
+            $('#movies').html(output);
+
+        })
+        .catch((err) => {
+            console.log(err);
     });
 }
