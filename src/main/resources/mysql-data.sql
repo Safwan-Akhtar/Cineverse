1fe15a3dfe4f3b902c36af3118cedac3e884b666
Chrisctr <credfern96@gmail.com>
2020-05-28 14:09:38 +0100

    Completed pre-populated SQL

--- src/main/resources/mysql-data.sql
+++ src/main/resources/mysql-data.sql
@@ -1,1 +1,27 @@
-INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T12:15:30', 'deluxe', 1, 'Mulan');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T12:15:00', 'deluxe', 1, 'Mulan');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T14:45:00', 'deluxe', 1, 'Frozen 2');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T18:45:00', 'deluxe', 1, 'Avengers: Infinity War');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T21:45:00', 'deluxe', 1, 'Avengers: Endgame');
+
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T12:00:00', 'deluxe', 2, 'Frozen 2');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T14:15:00', 'deluxe', 2, 'How To Train Your Dragon: Hidden World');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T17:15:00', 'deluxe', 2, 'Spiderman: Into The Spiderverse');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T20:15:00', 'deluxe', 2, 'Frozen 2');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T22:40:00', 'deluxe', 2, 'Mulan');
+
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T12:15:00', 'standard', 3, 'How To Train Your Dragon: Hidden World');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T14:45:00', 'standard', 3, 'Avengers: Endgame');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T18:15:00', 'standard', 3, 'Mulan');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T21:15:00', 'standard', 3, 'Frozen 2');
+
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T12:00:00', 'standard', 4, 'How To Train Your Dragon: Hidden World');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T14:55:00', 'standard', 4, 'Mulan');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T17:15:00', 'standard', 4, 'Spiderman: Into The Spiderverse');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T20:15:00', 'standard', 4, 'How To Train Your Dragon: Hidden World');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T22:35:00', 'standard', 4, 'Spiderman: Into The Spiderverse');
+
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T12:15:00', 'standard', 5, 'Spiderman: Into The Spiderverse');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T14:35:00', 'standard', 5, 'Spiderman: Into The Spiderverse');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T16:55:00', 'standard', 5, 'Spiderman: Into The Spiderverse');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T19:15:00', 'standard', 5, 'Spiderman: Into The Spiderverse');
+INSERT INTO screenings (movieDateTime, screenType, screenNumber, movieName) VALUES ('2020-06-10T21:45:00', 'standard', 5, 'Spiderman: Into The Spiderverse');
