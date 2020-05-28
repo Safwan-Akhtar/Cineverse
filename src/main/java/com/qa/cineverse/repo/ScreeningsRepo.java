09299013cf0012980cd5128c9a256f9af7a29698
Chrisctr <credfern96@gmail.com>
2020-05-28 22:13:58 +0100

    Added command to Screening Repo to try searching by name

--- src/main/java/com/qa/cineverse/repo/ScreeningsRepo.java
+++ src/main/java/com/qa/cineverse/repo/ScreeningsRepo.java
@@ -4,6 +4,9 @@ import com.qa.cineverse.domain.Screenings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScreeningsRepo extends JpaRepository<Screenings, Long> {
+    List<Screenings> findByName(String name);
 }
