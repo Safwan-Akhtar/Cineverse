10f883a9959f03dbe8891a28052a6af73ae4e346
Chrisctr <credfern96@gmail.com>
2020-05-29 16:39:44 +0100

    Fixed tickets repo

--- src/main/java/com/qa/cineverse/repo/TicketsRepo.java
+++ src/main/java/com/qa/cineverse/repo/TicketsRepo.java
@@ -1,9 +1,10 @@
 package com.qa.cineverse.repo;
 
 import com.qa.cineverse.domain.Screenings;
+import com.qa.cineverse.domain.Tickets;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketsRepo extends JpaRepository<Screenings, Long> {
+public interface TicketsRepo extends JpaRepository<Tickets, Long> {
 }
