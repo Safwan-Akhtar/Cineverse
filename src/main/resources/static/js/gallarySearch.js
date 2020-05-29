97f62dec046444d27c8d8a2c20e9d69aa4a83891
femarleycode <femarleycode@gmail.com>
2020-05-29 10:49:58 +0100

    search function and extra details functioning

--- src/main/resources/static/js/gallarySearch.js
+++ src/main/resources/static/js/gallarySearch.js
@@ -31,17 +31,17 @@ $(document).ready(() => {
 function getMovies(searchText) {
     axios.get(`http://www.omdbapi.com/?apikey=c737e3a5&s=`+searchText)
         .then((response) => {
-            console.log(response);
-            let movies = response.data.search;
+            console.log(response.data.Search);
+            let movies = response.data.Search;
             let output = '';
             //underscore removes an error message
-            $.each(movies = (_gallarySearch, movie) => {
+            $.each(movies, (_gallarySearch, movie) => {
                 output += `
                 <div classes="col-md-3">
                     <div class="well text-center">
                         <img src="${movie.Poster}">
                         <h5>${movie.Title}</h5>
-                        <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
                     </div>
                 </div>
                 `;
@@ -55,3 +55,61 @@ function getMovies(searchText) {
     });
 }
 
+
+function movieSelected(id) {
+    sessionStorage.setItem("movieId", id);
+    window.location = "gallarySearchInfo.html";
+    return false;
+}
+
+
+//Individual movie page info
+
+function getMovie() {
+    let movieId = sessionStorage.getItem("movieId");
+
+    axios.get(`http://www.omdbapi.com/?apikey=c737e3a5&i=` + movieId)
+        .then((response) => {
+            console.log(response.data);
+            let movie = response.data;
+
+            let output = `
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="${movie.Poster}" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <h2>${movie.Title}</h2>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                        <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+                <div class="row">
+                    <div class="well">
+                        <h3>Plot</h3>
+                        ${movie.Plot}
+                        <hr>
+                        <a href="http://idmdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                        <a hred="gallarySearch.html" class="btn btn-default">Go back to search</a>
+                    </div>
+            </div>
+            `;
+
+            $("#movie").html(output);
+        })
+
+        
+        .catch((err) => {
+            console.log(err);
+        });
+
+}
