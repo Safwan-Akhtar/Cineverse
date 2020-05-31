9b491fa868669931d8144cecc5ea7357fb30fb03
Chrisctr <credfern96@gmail.com>
2020-05-31 17:21:41 +0100

    Redirected controller mapping for allPermit to index.html

--- src/main/java/com/qa/cineverse/controller/HomeResource.java
+++ src/main/java/com/qa/cineverse/controller/HomeResource.java
@@ -1,14 +1,17 @@
 package com.qa.cineverse.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeResource {
 
-    @GetMapping("/")
-    public String home() {
-        return ("<h1>Welcome</h1>");
+    @RequestMapping(value = "/assessment", method = RequestMethod.GET)
+    public String index() {
+        return "index";
     }
 
     @GetMapping("/user")  ///Ticket Bookings and Seat Selector
