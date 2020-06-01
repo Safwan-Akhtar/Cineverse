d73def644315c4e44fe2ecc7748f849814ad2700
femarleycode <femarleycode@gmail.com>
2020-06-01 11:16:53 +0100

    selenium files added

---
+++ src/test/java/com/qa/cineverse/selenium/seltestfel.java
@@ -0,0 +1,11 @@
+
+
+public class seltestfel {
+
+   public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\Users\felix\Automation Tests\chromedriver.exe")
+        WebDriver driver = ChromeDriver();
+
+   }
+
+}
