e27f02821446b666a04e9f5494c672fe966b7bd0
Chrisctr <credfern96@gmail.com>
2020-06-01 18:31:37 +0100

    Added more seat stuff

--- src/main/resources/static/js/seat.js
+++ src/main/resources/static/js/seat.js
@@ -1,3 +1,35 @@
 $('.cinema-seats .seat').on('click', function () {
     $(this).toggleClass('active');
   });
+
+
+
+  
+
+  var inactive = document.getElementsByClassName(".cinema-seats .seat").length;
+  inactive.valueOf()
+  console.log(inactive)
+
+  var active = document.getElementsByClassName(".cinema-seats .seat active").length;
+  active.valueOf()
+  console.log(active)
+
+
+  function getSeatValue() {
+    element = document.getElementsByClassName(".cinema-seats .seat");
+    
+    let count = 0;
+
+    array.forEach(element => {
+        console.log(count);
+        count++;
+    });
+
+
+  }
+
+
+
+  let countSeats = document.querySelector('#countSeats');
+  countSeats.addEventListener('click', seatScan);
+
