f087b1c0209f1c02c069a94d62994fc9bbf77c2e
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-06-01 18:58:41 +0100

    temp fixed Seltestfel.java so I can run the program

--- src/test/java/com/qa/cineverse/selenium/Seltestfel.java
+++ src/test/java/com/qa/cineverse/selenium/Seltestfel.java
@@ -1,11 +1,13 @@
-package com.qa.selenium.seltestfel;
+package com.qa.cineverse.selenium;
 
+import com.relevantcodes.extentreports.ExtentTest;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 
 public class Seltestfel {
     WebDriver driver;
-    ExtentsReports report;
+    //ExtentsReports report;
     ExtentTest test;
 
     @BeforeTest
@@ -18,8 +20,8 @@ public class Seltestfel {
 
 
    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\felix\\Automation Tests\\chromedriver.exe")
-        WebDriver driver = ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\felix\\Automation Tests\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
         driver.get("http://www.gmail.com/");
         driver.manage().window().maximize();
 
