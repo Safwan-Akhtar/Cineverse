f2326b023840e8a7dd18699702187b9bf87f0346
Chrisctr <credfern96@gmail.com>
2020-06-01 10:36:44 +0100

    Added Chromedriver properly

--- src/main/java/com/qa/cineverse/configuration/SecurityConfiguration.java
+++ src/main/java/com/qa/cineverse/configuration/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -38,6 +37,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() { return NoOpPasswordEncoder.getInstance (); }  /// Need to change to actual encoder
-
-
 }
