05327f40577260015a639fe7e9db217f8fdf854a
Chrisctr <credfern96@gmail.com>
2020-06-02 15:44:45 +0100

    Made more tests for UserDTO

--- src/test/java/com/qa/cineverse/dto/UserDTOTest.java
+++ src/test/java/com/qa/cineverse/dto/UserDTOTest.java
@@ -35,21 +35,34 @@ public class UserDTOTest {
         userOther.setActive(false);
         userOther.setEmail("braffloverxx@yahoo.com");
         userOther.setRoles("ROLE_ADMIN");
+
+        userDTO = new UserDTO(user);
+        other = new UserDTO(userOther);
     }
 
-    @Test(expected=NullPointerException.class)
+    @Test
     public void userDTOForenameNullButOtherForenameNotNull() {
         userDTO.setForename(null);
         assertFalse(userDTO.equals(other));
     }
 
     @Test
-    public void userDTOForenamesNotEqual() {
-        other.setForename("braff");
+    public void userDTOForenameNotEqual() {
+        userDTO.setForename("Boyman");
         assertFalse(userDTO.equals(other));
     }
 
+    @Test
+    public void userDTOSurnameNullButOtherForenameNotNull() {
+        userDTO.setSurname(null);
+        assertFalse(userDTO.equals(other));
+    }
 
+    @Test
+    public void userDTOSurnameNotEqual() {
+        userDTO.setSurname("Chris");
+        assertFalse(userDTO.equals(other));
+    }
 
 
 }
