92dd1dc4a6b63305edbd4764a65917adc5ec6e65
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-06-02 22:22:33 +0100

    gotten an array of selected seat id's, moved button outside section

--- src/main/resources/static/js/seat.js
+++ src/main/resources/static/js/seat.js
@@ -1,58 +1,28 @@
 
-// let navTest = document.getElementsByClassName("nav-item active");
-// console.log(navTest);
-//
-// let navTest2 = document.getElementsByClassName("nav-item");
-// console.log(navTest2);
-
-let inactive = document.getElementsByClassName("seat");
-  inactive.valueOf();
-  console.log(inactive);
-  console.log(inactive.length); // 0
-  console.log(inactive.valueOf()); //  HTML Collection []
-
-  let active = document.getElementsByClassName("seat active");
-  active.valueOf();
-  console.log(active); // HTML Collection []
-  console.log(active.length); // 0
-  console.log(active.valueOf()); // HTML Collection []
-
-
+// we want to know number of seats selected
+// we want the id's of the seats selected
   function getSeatValue() {
     console.log("getSeatValue() triggered")
 
-    let inactive = document.getElementsByClassName("seat");
-    inactive.valueOf();
-
-    console.log(inactive); // HTML Collection []
-    console.log(inactive.length); // 0
-    console.log(inactive.valueOf()); //  HTML Collection []
-
+    let allSeats = document.getElementsByClassName("seat");
     let active = document.getElementsByClassName("seat active");
-    active.valueOf();
+
     console.log(active); // HTML Collection []
-    console.log(active.length); // 0
-    console.log(active.valueOf()); // HTML Collection []
+    console.log(active.length); // total seats selected
+    console.log(active[0]);
+    console.log(active[0].id); // HTML Collection []
 
     console.log("--------------")
 
-    let element = document.getElementsByClassName("seat");
-    let count = 0;
-    console.log(element);
+    let arrSimple = Array.from(active);
+    console.log(arrSimple);
 
-    // we want to know number of seats selected
-    // we want the id's of the seats selected
-
-    array.forEach(element => {
-      if (element.className === "cinema-seats seat active") {
-        count++;
-        console.log(count);
-        let seatNo = element.id;
-        console.log(seatNo);
-      } else {
-        //do nothing
-      }
-    });
+    let seat = ``;
+    for (let i = 0; i < active.length; i++){
+      seat += `${arrSimple[i].id},`;
+      console.log(seat);
+    }
+    console.log(seat);
 
   }
 
