33989fea3043bc74b45e0b01c20d0410752ff741
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-06-02 21:40:50 +0100

    added and commented a nav bar test for getElementsByClassName

--- src/main/resources/static/js/seat.js
+++ src/main/resources/static/js/seat.js
@@ -1,27 +1,50 @@
 
-  let inactive = document.getElementsByClassName(".cinema-seats .seat").length;
-  inactive.valueOf();
-  console.log(inactive); //0
-  console.log(inactive.valueOf()); //0
+// let navTest = document.getElementsByClassName("nav-item active");
+// console.log(navTest);
+//
+// let navTest2 = document.getElementsByClassName("nav-item");
+// console.log(navTest2);
 
-  let active = document.getElementsByClassName(".cinema-seats .seat active").length;
-  active.valueOf()
-  console.log(active) //0
-  console.log(active.valueOf()); //0
+let inactive = document.getElementsByClassName("seat");
+  inactive.valueOf();
+  console.log(inactive);
+  console.log(inactive.length); // 0
+  console.log(inactive.valueOf()); //  HTML Collection []
+
+  let active = document.getElementsByClassName("seat active");
+  active.valueOf();
+  console.log(active); // HTML Collection []
+  console.log(active.length); // 0
+  console.log(active.valueOf()); // HTML Collection []
 
 
   function getSeatValue() {
     console.log("getSeatValue() triggered")
-    let element = document.getElementsByClassName(".cinema-seats .seat");
+
+    let inactive = document.getElementsByClassName("seat");
+    inactive.valueOf();
+
+    console.log(inactive); // HTML Collection []
+    console.log(inactive.length); // 0
+    console.log(inactive.valueOf()); //  HTML Collection []
+
+    let active = document.getElementsByClassName("seat active");
+    active.valueOf();
+    console.log(active); // HTML Collection []
+    console.log(active.length); // 0
+    console.log(active.valueOf()); // HTML Collection []
+
+    console.log("--------------")
+
+    let element = document.getElementsByClassName("seat");
     let count = 0;
     console.log(element);
 
     // we want to know number of seats selected
     // we want the id's of the seats selected
 
-
     array.forEach(element => {
-      if (element.className === ".cinema-seats .seat active") {
+      if (element.className === "cinema-seats seat active") {
         count++;
         console.log(count);
         let seatNo = element.id;
@@ -34,6 +57,5 @@
   }
 
   let countSeatsHash = document.querySelector('#countSeats');
-  console.log(countSeatsHash);
   countSeatsHash.addEventListener('click', getSeatValue);
 
