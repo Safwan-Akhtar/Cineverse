ca6387939145b2c92dd18858875f58b2963e141f
Chrisctr <credfern96@gmail.com>
2020-06-03 00:42:01 +0100

    Working on register validation

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -7,28 +7,43 @@ const registerUser = () => {
     let password = document.getElementById("password").value;
     let passwordMatch = document.getElementById("passwordMatch").value;
 
-    axios({
-        method: 'post',
-        url: 'http://localhost:8181/createUser',
-        data: `{
-            "username" : "${username}",
-            "password" : "${password}",
-            "matchingPassword" : "${passwordMatch}",
-            "roles" : "ROLE_USER",
-            "active" : "true",
-            "email" : "${email}",
-            "forename" : "${forename}",
-            "surname" : "${surname}"
-        }`,
-        headers: {'Content-Type': 'application/json' }
-        })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (response) {
-            console.log(response);
-        }); 
+
+        axios({
+            method: 'post',
+            url: 'http://localhost:8181/createUser',
+            data: `{
+                "username" : "${username}",
+                "password" : "${password}",
+                "matchingPassword" : "${passwordMatch}",
+                "roles" : "ROLE_USER",
+                "active" : "true",
+                "email" : "${email}",
+                "forename" : "${forename}",
+                "surname" : "${surname}"
+            }`,
+            headers: {'Content-Type': 'application/json' }
+            })
+            .then(function (response) {
+                console.log(response);
+                
+            })
+            .catch(function (response) {
+                console.log(response);
+            }); 
 }
 
-let registerButton = document.querySelector('#registerButton');
-registerButton.addEventListener('click', registerUser);
+function check(input) {
+    if (input.value != document.getElementById('password').value) {
+        input.setCustomValidity('Password Must be Matching.');
+    } else {
+        // input is valid -- reset the error message
+        input.setCustomValidity('');
+        let registerButton = document.querySelector('#registerButton');
+        registerButton.addEventListener('click', registerUser);
+    }
+}
+
+
+
+
+
