a40806d2a9792478ac5c5d9dac349add993b98f9
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-06-02 15:59:12 +0100

    moved js for seat colours to the html/iframe

--- src/main/resources/static/js/seat.js
+++ src/main/resources/static/js/seat.js
@@ -1,7 +1,3 @@
-// For active visual status
-$('.cinema-seats .seat').on('click', function () {
-    $(this).toggleClass('active');
-  });
 
   var inactive = document.getElementsByClassName(".cinema-seats .seat").length;
   inactive.valueOf()
@@ -14,24 +10,20 @@ $('.cinema-seats .seat').on('click', function () {
 
   function getSeatValue() {
     let element = document.getElementsByClassName(".cinema-seats .seat");
-    
     let count = 0;
+    console.log(element);
 
     array.forEach(element => {
       if (element.className === ".cinema-seats .seat active") {
-        console.log(count);
         count++;
+        console.log(count);
       } else {
         //do nothing
       }
-
     });
-
 
   }
 
-
-
   let countSeats = document.querySelector('#countSeats');
-  countSeats.addEventListener('click', seatScan);
+  countSeats.addEventListener('click', getSeatValue);
 
