9f308e7e1ef9fd9ddaa2fb8390f32a8ad6068877
Chrisctr <credfern96@gmail.com>
2020-06-02 18:23:50 +0100

    Added register.js

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,9 +1,34 @@
   
 const registerUser = () => {
+    let forename = document.getElementById("forename").value;
+    let surname = document.getElementById("surname").value;
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let passwordMatch = document.getElementById("passwordMatch").value;
 
+    axios({
+        method: 'post',
+        url: 'http://localhost:8181/createAbilities',
+        data: `{
+            "username" : ""${username}",
+            "password" : ""${password}",
+            "matchingPassword" : ""${passwordMatch}",
+            "roles" : "ROLE_USER",
+            "active" : "true",
+            "email" : ""${email}",
+            "forename" : ""${forename}",
+            "surname" : ""${surname}"
+        }`,
+        headers: {'Content-Type': 'application/json' }
+        })
+        .then(function (response) {
+            console.log(newId);
+        })
+        .catch(function (response) {
+            console.log(response);
 
-
-
+        }); 
 }
 
 let registerButton = document.querySelector('#registerButton');
