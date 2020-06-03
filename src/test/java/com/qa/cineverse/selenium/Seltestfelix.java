5fea895d89731a69492e71f7b5ced202a7196947
femarleycode <femarleycode@gmail.com>
2020-06-03 11:56:57 +0100

    tidying

--- src/test/java/com/qa/cineverse/selenium/Seltestfelix.java
+++ src/test/java/com/qa/cineverse/selenium/Seltestfelix.java
@@ -25,6 +25,8 @@ import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
@@ -102,119 +104,42 @@ public class Seltestfelix {
 
     }
 
+
+
+
     @After
     public void tearDown() {
         this.driver.quit();
     }
 }
 
-//        WebElement name = driver.findElement(By.id("createName"));
-//        name.sendKeys("Donald");
-//        WebElement colour = driver.findElement(By.id("createColour"));
-//        colour.sendKeys("White");
-//        WebElement habitat = driver.findElement(By.id("createHabitat"));
-//        habitat.sendKeys("Donald");
-//
-//        WebElement button = driver.findElement(By.id("createButton"));
-//        button.click();
-//
-//        WebElement output = driver.findElement(By.id("createOutput"));
-//
-//        assertFalse(output.getText().isBlank());
-//    }
-//
-//
 
-//
-//}
-//
-//
-//
-//
-//
-//
-//
-//    @BeforeTest
-//    public void startReport(){
-//        report = new ExtentReports(
-//                System.getProperty("user.dir") + "/test-output/Report.html",
-//                true
-//        );
-//        report
-//                .addSystemInfo("Host Name", "QA")
-//                .addSystemInfo("Tester", "Felix");
-//        report.loadConfig(new File(System.getProperty("user.dir") + "\\extent-report.xml"));
-//    }
-//
-//    @BeforeMethod
-//    public void setUp(){
-//        SpringApplication.run(App.class);
-//        System.setProperty("webdriver.chrome.driver", "chromedriver");
-//        driver = new ChromeDriver();
-//    }
-//
-//    public void openChrome() {
-//        driver.manage().window().maximize();
-//        test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
-//        driver.get("http://localhost:8182/");
-//        test.log(LogStatus.INFO, "Navigating to the application web interface");
-//    }
-//
+
+
+
+
+
+
+//// choose random option from drop downs - alternatively use loop?
 //    @Test
-//    public void seleniumDrawHouse() throws InterruptedException{
-//        driver.manage().window().maximize();
-//        sleep( 2000);
-//        driver.get("https://www.youidraw.com/apps/painter/");
-//        sleep ( 2000);
+//    public void randomDropDown() throws InterruptedException {
 //
-//        Actions action = new Actions(driver);
-//        sleep( 2000);
+//    test = report.startTest("Verifying the title of Cineverse website");
+//    driver.manage().window().maximize();
 //
+//    // Locate the dropdown menu
+//    WebElement dropdown = (WebElement) driver.findElements(By.id("infoDrop"));
+//    dropdown.click();
 //
-////    @Test
-////    public void clickClassPage() throws InterruptedException, IOException {
-////        test = report.startTest("Verifying Navigation to Create User Page");
-////        openChrome();
-////        sleep(5000);
-////        File webAppPic = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-////        FileUtils.copyFile(webAppPic, new File(System.getProperty("user.dir") + "/test-output/webAppHome.jpg"));
-////        test.log(LogStatus.INFO, "Navigated to the web interface", "<img src=webAppHome.jpg>");;
-////        WebElement pageHeader = driver.findElement(By.id("header"));
-////        assertEquals(pageHeader.getText(), "Welcome to Cineverse");
+//    //Get the list of dropdown options
+//    List<WebElement> itemsInDropdown = driver.findElements(By.id("id of the dropdown list"));
 //
+//    // Get the size of dropdown list
+//    int size = itemsInDropdown.size();
 //
+//    // Generate the random number
+//    int randomNumber = ThreadLocalRandom.current().nextInt(0, size);
 //
-//
-////        WebElement createUserLink = driver.findElement(By.id("userCreation"));
-//
-////        FileUtils.copyFile(webAppPic, new File(System.getProperty("user.dir") + "/test-output/webAppHome.jpg"));
-////        test.log(LogStatus.INFO, "Navigated to the web interface", "<img src=webAppHome.jpg>");
-////        sleep(2000);
-////        WebElement createUserLink = driver.findElement(By.id("userCreation"));
-////        createUserLink.click();
-////        test.log(LogStatus.INFO, "Click on userCreation link");
-////        sleep(2000);
-////        WebElement pageHeader = driver.findElement(By.id("pageHeader"));
-////        assertEquals(pageHeader.getText(), "GameTimeLog - Create a User");
-////        File userCreationPic = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-//
-//   }
-//
-//   @AfterMethod
-//    public void getResults(ITestResult result){
-//        driver.close();
-//        if(result.getStatus() == ITestResult.FAILURE){
-//            test.log(LogStatus.FAIL, "Test has failed" + result.getName());
-//            test.log(LogStatus.FAIL, "Test has failed" + result.getThrowable());
-//        } else if (result.getStatus() == ITestResult.SUCCESS) {
-//            test.log(LogStatus.PASS, "Test has passed" + result.getName());
-//        }
-//        report.endTest(test);
-//   }
-//   @AfterTest
-//    public void endReport(){
-//        report.flush();
-//        report.close();
-//   }
-//
+//    // Clicking on random value
+//    itemsInDropdown.get(randomNumber).click();
 //}
