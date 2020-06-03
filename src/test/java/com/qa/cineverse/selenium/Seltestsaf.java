6ad97d97d79a271cbf4db3e2eb9cf16f2f9dc7fc
Safwan-Akhtar <safwan.qa@hotmail.com>
2020-06-03 10:31:11 +0100

    added id and fixed link for selenium testing.

---
+++ src/test/java/com/qa/cineverse/selenium/Seltestsaf.java
@@ -0,0 +1,71 @@
+package com.qa.cineverse.selenium;
+
+import static java.lang.Thread.*;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Seltestsaf {
+
+
+
+    @LocalServerPort
+    private int port;
+    private WebDriver driver;
+    ExtentReports report;
+    ExtentTest test;
+
+    @Before
+    public void init(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        ChromeOptions opts = new ChromeOptions();
+        this.driver = new ChromeDriver();
+
+        report = new ExtentReports(
+                System.getProperty("user.dir") + "/test-output/Report.html",
+                true
+        );
+        report
+                .addSystemInfo("Host Name", "QA")
+                .addSystemInfo("Tester", "Saf");
+        report.loadConfig(new File(System.getProperty("user.dir") + "\\extent-report.xml"));
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        test = report.startTest("Verifying the title of Cineverse website");
+        driver.manage().window().maximize();
+        test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
+        driver.get("http://localhost:" + port);
+        test.log(LogStatus.INFO, "Navigating to the Cineverse website");
+        WebElement header = driver.findElement(By.id("header"));
+        assertTrue(header.isDisplayed());
+        test.log(LogStatus.PASS, "The title was exactly the same");
+        sleep(4000);
+
+    }
+
+    @After
+    public void teardown(){
+        this.driver.quit();
+    }
+
+}
