d43f3a26be42b783eba475a6122f947f8d12b50d
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-06-03 22:46:00 +0100

    added complete view tickets card creator from get reqs

--- src/main/resources/static/js/viewTickets.js
+++ src/main/resources/static/js/viewTickets.js
@@ -8,8 +8,14 @@ axios.get(`http://localhost:8181/getCustomerById/${id}`, configGet)
     .then(function (response) {
         let ticketsArr = response.data.tickets;
         console.log(response.data.tickets);
-
-        populateTicketsDiv(ticketsArr);
+        let screeningId = ticketsArr[0].screenId;
+        axios.get(`http://localhost:8181/getScreeningById/${screeningId}`, configGet)
+            .then(function (response) {
+                let screeningData = response.data;
+                populateTicketsDiv(ticketsArr, screeningData);
+        })
+            .catch(function (error) {
+                console.log(error);
     })
     .catch(function (error) {
         console.log(error);
@@ -18,66 +24,73 @@ axios.get(`http://localhost:8181/getCustomerById/${id}`, configGet)
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
-function populateTicketsDiv(tickets) {
-    const div = document.querySelector("#bookingResponse");
-    for (let ticket of tickets) {
+// set Prices
 
+
+
+function populateTicketsDiv(tickets, screening) {
+    const div = document.querySelector("#bookingResponse");
+
+    for (let ticket of tickets) {
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
 
-        // ticket has ticketId, ticketType, seatNo, screenId, userId
-        // screenId links to movie Name / Date/Time & ScreenType
+        const cardBody = document.createElement("div");
+        cardBody.setAttribute('class', 'card-body');
+        const cardBodyTwo = document.createElement("ul");
+        cardBodyTwo.setAttribute('class', 'list-group list-group-flush');
 
-        //   class="card-body" for div containing text
-        //   class="card-title" for movie title
-        //   class="card-text" for actors / directors
-        // or "list-group-item" if you change to li
-
-        const article = document.createElement("article");
-        const divTwo = document.createElement("div");
-        const image = document.createElement("img");
-        const divThree = document.createElement("div");
-        const divFour = document.createElement("div");
-        const header = document.createElement("header");
-        const hTwo = document.createElement("h2");
-        const aTag = document.createElement("a");
-        const pTag = document.createElement("p");
-        const pTagOne = document.createElement("p");
-        const pTagTwo = document.createElement("p");
+        // ticket has ticketId, ticketType, seatNo, screenId (same as screen's screeningsId), userId
+        const pTag = document.createElement("li");
+        pTag.setAttribute('class', 'list-group-item');
+        const pTagOne = document.createElement("li");
+        pTagOne.setAttribute('class', 'list-group-item');
+        const pTagTwo = document.createElement("li");
+        pTagTwo.setAttribute('class', 'list-group-item');
+        // userId
         const pTagThree = document.createElement("p");
+        pTag.setAttribute('class', 'text-muted');
 
+        pTag.textContent = "SeatNo: " + ticket.seatNo;
+        pTagOne.textContent = "Type: " + ticket.ticketType; //adult/child/student
+        pTagTwo.textContent = "Price: " + price;
+        pTagThree.textContent = "Buyer id: " + ticket.userId;
 
-        article.id = movie.imdbID;
+        // screenId links to movie Name / Date/Time, ScreenNumber & ScreenType
+        let movieDateTime = screening.movieDateTime;
+        let movieDate = movieDateTime.substring(0,10);
+        let movieTime = movieDateTime.substring(11,16);
 
-        image.src = movie.Poster;
-        image.position = "centre";
-        image.alt = `${movie.Title} poster`;
+        const hTwoMovie = document.createElement("h2");
+        hTwoMovie.setAttribute('class', 'card-header');
+        hTwoMovie.textContent = `${screening.movieName}`;
 
+        const hFourTime = document.createElement("h4");
+        hFourTime.setAttribute('class', 'card-title');
+        hFourTime.textContent = `${movieTime}`;
 
-        // Link to details will be in title (h2) tag - can move to a button
-        hTwo.id = movie.imdbID;
-        aTag.href = "#";
-        //aTag.onclick = `movieListingSelected('${movie.imdbID}')`;
-        aTag.setAttribute('onclick', `movieListingSelected('${movie.imdbID}')`);
-        aTag.textContent = movie.Title;
+        const hFourScreen = document.createElement("h4");
+        hFourScreen.setAttribute('class', 'card-subtitle mb-2 text-muted');
+        hFourScreen.textContent = `Screen: ${screening.screenNumber} (${screening.screenType})`;
 
+        const date = document.createElement("p");
+        date.setAttribute('class', 'card-text');
+        date.textContent = `${movieDate}`;
 
-        pTag.textContent = "Release Date: " + movie.Released;
-        pTagOne.textContent = "Cast: " + movie.Actors;
-        pTagTwo.textContent = "Director: " + movie.Director;
-        pTagThree.textContent = "Age Rating: " + movie.Rated;
+        card.id = `Ticket${ticket.id}`;
 
-        hTwo.appendChild(aTag);
-        header.appendChild(hTwo);
-        header.appendChild(pTag);
-        header.appendChild(pTagOne)
-        header.appendChild(pTagTwo);
-        header.appendChild(pTagThree);
-        divFour.appendChild(header);
-        divThree.appendChild(divFour);
-        divTwo.appendChild(image);
-        article.appendChild(divTwo);
-        article.appendChild(divThree);
-        div.appendChild(article);
+        cardBodyTwo.appendChild(pTag);
+        cardBodyTwo.appendChild(pTagOne);
+        cardBodyTwo.appendChild(pTagTwo);
+        cardBodyTwo.appendChild(pTagThree);
+        cardBody.appendChild(hTwoMovie);
+        cardBody.appendChild(hFourTime);
+        cardBody.appendChild(hFourScreen);
+        cardBody.appendChild(date);
+
+        card.appendChild(cardBody);
+        card.appendChild(cardBodyTwo);
+        div.appendChild(card);
     }
+
 }
